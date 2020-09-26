1274ae594cc4e451ae243bda9ed85fcba899c5ee
Serghei Nuta <sergiu.nuta@gmail.com>
2020-09-26 19:00:17 +0100

    fixed build command in gitignore

---
+++ build/precache-manifest.d81307520809f069f5dff0558346113e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b12e9600a6a70f4118f88a803b801c9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21e0069daa59c8358fc5",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3f385b9ce65f742069f1",
+    "url": "/static/css/main.cfc00448.chunk.css"
+  },
+  {
+    "revision": "21e0069daa59c8358fc5",
+    "url": "/static/js/2.49ec8050.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.49ec8050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f385b9ce65f742069f1",
+    "url": "/static/js/main.0cf8364c.chunk.js"
+  },
+  {
+    "revision": "c2d4045e350c426afa65",
+    "url": "/static/js/runtime-main.5666197e.js"
+  },
+  {
+    "revision": "513c07fef0c76a2d11dc7618a5cbed8b",
+    "url": "/static/media/IMG_9210.513c07fe.jpg"
+  },
+  {
+    "revision": "ad107c3369f3076969fd6dcc07a94061",
+    "url": "/static/media/IMG_9212.ad107c33.jpg"
+  },
+  {
+    "revision": "68890804719479324261ae825ec09da4",
+    "url": "/static/media/calm.68890804.png"
+  },
+  {
+    "revision": "386509fcdae473ac10f1d68e2c2652d9",
+    "url": "/static/media/dwayne.386509fc.jpg"
+  },
+  {
+    "revision": "6d63b80df99525b4383e068b71a8c3b1",
+    "url": "/static/media/smart-scale.6d63b80d.png"
+  }
+]);
